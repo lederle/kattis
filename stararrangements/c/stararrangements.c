95dfab9a5a54735e042d574200dd2bc82458c1d1
Mike Lederle <mrlederle@gmail.com>
2023-08-19 17:57:52 -0400

    stararrangements

--- stararrangements/c/stararrangements.c
+++ stararrangements/c/stararrangements.c
@@ -6,7 +6,15 @@ int main(void) {
     int S;
     if (scanf("%d", &S) != 1)
         return EXIT_FAILURE;
-    
+
+    printf("%d:\n", S);
+    for (int R = 2; R <= S / 2 + 1; ++R) {
+        if (S % (2 * R - 1) == 0 || (S + R - 1) % (2 * R - 1) == 0)
+            printf("%d,%d\n", R, R - 1);
+
+        if (S % R == 0)
+            printf("%d,%d\n", R, R);
+    }
 
     return EXIT_SUCCESS;
 }
