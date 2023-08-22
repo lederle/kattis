c60cb060094dcf59621a08b5b82325931d14988a
Mike Lederle <mrlederle@gmail.com>
2023-08-22 16:55:09 -0400

    overdraft

--- overdraft/c/overdraft.c
+++ overdraft/c/overdraft.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "liboverdraft.h"
-
-// 
+#include <assert.h>
+#include <limits.h>
+// #include "liboverdraft.h"
 
 int main(void) {
+    int n;
+    assert(scanf("%d", &n) == 1);
 
+    long t, min_bal = LONG_MAX, bal = 0;
+    while (n--) {
+        assert(scanf("%ld", &t) == 1);
+        bal += t;
+        if (bal < min_bal)
+            min_bal = bal;
+    }
+    if (min_bal < 0)
+        printf("%ld\n", -min_bal);
+    else
+        printf("0\n");
     return EXIT_SUCCESS;
 }
