0f40c4ebef5abb1c60752be6ce860306caad7281
Mike Lederle <mrlederle@gmail.com>
2023-08-25 13:21:15 -0400

    match detected (NB haven't considered center letter yet)

--- spellingbee/c/libspellingbee.c
+++ spellingbee/c/libspellingbee.c
@@ -9,9 +9,12 @@ char **spellingbee(char letters[], char *dict[], int dict_size) {
   int size = 2, match_count = 0;
   char **matches = malloc(size * sizeof(*matches));
   assert(matches);
-  // find matches
+
   for (int dict_iter = 0; dict_iter < dict_size; dict_iter++) {
-    // if dict[i] matches, malloc space and add to ret
+    size_t span = strspn(dict[dict_iter], letters);
+    if (dict[dict_iter][span] != '\0') continue;
+
+    match_count++;
     char *match = dict[dict_iter];
     while (match_count >= size) {
       size *= 2;
@@ -19,10 +22,11 @@ char **spellingbee(char letters[], char *dict[], int dict_size) {
       assert(matches);
     }
     int slen = strlen(match) + 1;
-    matches[match_count] = malloc(slen * sizeof(char));
-    sprintf(matches[match_count], "%s", match);
-    match_count++;
+    matches[match_count - 1] = malloc(slen * sizeof(char));
+    sprintf(matches[match_count - 1], "%s", match);
   }
+  matches[match_count] = malloc(3 * sizeof(char));
+  sprintf(matches[match_count], "%s", "-1");
 
   return matches;
 }
--- spellingbee/c/test_spellingbee.c
+++ spellingbee/c/test_spellingbee.c
@@ -17,40 +17,70 @@ static void test_one_match_size_one_dict(void) {
   char letters[7] = "abcdefg";
   int dict_size = 1;
   char *dict[] = {"abcd"};
-  char *expected[] = {"abcd"};
+  char *expected[] = {"abcd", "-1"};
   char **actual = spellingbee(letters, dict, dict_size);
 
-  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 1);
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 2);
 }
 
 static void test_two_matches_size_two_dict(void) {
   char letters[7] = "abcdefg";
   int dict_size = 2;
   char *dict[] = {"abcd", "acde"};
-  char *expected[] = {"abcd", "acde"};
+  char *expected[] = {"abcd", "acde", "-1"};
   char **actual = spellingbee(letters, dict, dict_size);
 
-  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 2);
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 3);
 }
 
 static void test_three_matches_size_three_dict(void) {
   char letters[7] = "abcdefg";
   int dict_size = 3;
   char *dict[] = {"abcd", "acde", "adef"};
-  char *expected[] = {"abcd", "acde", "adef"};
+  char *expected[] = {"abcd", "acde", "adef", "-1"};
   char **actual = spellingbee(letters, dict, dict_size);
 
-  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 3);
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 4);
 }
 
 static void test_five_matches_size_five_dict(void) {
   char letters[7] = "abcdefg";
   int dict_size = 5;
   char *dict[] = {"abcd", "acde", "adef", "abce", "abcf"};
-  char *expected[] = {"abcd", "acde", "adef", "abce", "abcf"};
+  char *expected[] = {"abcd", "acde", "adef", "abce", "abcf", "-1"};
   char **actual = spellingbee(letters, dict, dict_size);
 
-  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 5);
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 6);
+}
+
+static void test_one_match_size_two_dict(void) {
+  char letters[7] = "abcdefg";
+  int dict_size = 2;
+  char *dict[] = {"abcz", "abcd"};
+  char *expected[] = {"abcd", "-1"};
+  char **actual = spellingbee(letters, dict, dict_size);
+
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 2);
+}
+
+static void test_one_match_size_three_dict(void) {
+  char letters[7] = "abcdefg";
+  int dict_size = 3;
+  char *dict[] = {"abcz", "abcd", "abcy"};
+  char *expected[] = {"abcd", "-1"};
+  char **actual = spellingbee(letters, dict, dict_size);
+
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 2);
+}
+
+static void test_two_matches_size_three_dict(void) {
+  char letters[7] = "abcdefg";
+  int dict_size = 3;
+  char *dict[] = {"abcy", "abcd", "abce"};
+  char *expected[] = {"abcd", "abce", "-1"};
+  char **actual = spellingbee(letters, dict, dict_size);
+
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 3);
 }
 
 int main(void) {
@@ -60,5 +90,8 @@ int main(void) {
     RUN_TEST(test_two_matches_size_two_dict);
     RUN_TEST(test_three_matches_size_three_dict);
     RUN_TEST(test_five_matches_size_five_dict);
+    RUN_TEST(test_one_match_size_two_dict);
+    RUN_TEST(test_one_match_size_three_dict);
+    RUN_TEST(test_two_matches_size_three_dict);
     return UnityEnd();
 }
