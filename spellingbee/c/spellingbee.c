375e5c42c407b9068e0a23fea63ee71ddec931e5
Mike Lederle <mrlederle@gmail.com>
2023-08-25 11:34:11 -0400

    clang format test

--- spellingbee/c/spellingbee.c
+++ spellingbee/c/spellingbee.c
@@ -13,13 +13,13 @@ int main(void) {
     char word[25];
     char *dict[n];
     for (int i = 0; i < n; i++) {
-        // as alternate: read dict words, have spellingbee() determine
-        // if they match, print.
-        // It is a good exercise to pass the arrays and return the
-        // output in an array, but it ultimately is less clear?
-        assert(scanf("%s", word) == 1);
-        
-        dict[i] = word;
+      // as alternate: read dict words, have spellingbee() determine
+      // if they match, print.
+      // It is a good exercise to pass the arrays and return the
+      // output in an array, but it ultimately is less clear?
+      assert(scanf("%s", word) == 1);
+
+      dict[i] = word;
     }
 
     char **ret = spellingbee(letters, dict, n);
--- spellingbee/c/test_spellingbee.c
+++ spellingbee/c/test_spellingbee.c
@@ -1,6 +1,7 @@
+#include <math.h>
+
 #include "../../lib/test-framework/unity.h"
 #include "libspellingbee.h"
-#include <math.h>
 
 void setUp(void) {
 }
@@ -13,19 +14,19 @@ static void test_canary(void) {
 }
 
 static void test_one_match_size_one_dict(void) {
-    char letters[7] = "abcdefg";
-    int dict_size = 1;
-    char *dict[] = { "abcd" };
-    char *expected[] = { "abcd" };
-    char **actual = spellingbee(letters, dict, dict_size);
+  char letters[7] = "abcdefg";
+  int dict_size = 1;
+  char *dict[] = {"abcd"};
+  char *expected[] = {"abcd"};
+  char **actual = spellingbee(letters, dict, dict_size);
 
-    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 1);
+  TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 1);
 }
 
 int main(void) {
     UnityBegin("test_spellingbee.c");
     RUN_TEST(test_canary);
     RUN_TEST(test_one_match_size_one_dict);
-    
+
     return UnityEnd();
 }
