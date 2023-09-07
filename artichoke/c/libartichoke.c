84c3fd3543b095623b35b7b64f77f9a139524b8b
Mike Lederle <mrlederle@gmail.com>
2023-09-07 11:44:58 -0400

    artichoke

--- artichoke/c/libartichoke.c
+++ artichoke/c/libartichoke.c
@@ -1,3 +1,18 @@
 #include "libartichoke.h"
 
-double artichoke(int p, int a, int b, int c, int d, int n) { return 1.0; }
+#include <math.h>
+
+double artichoke(int p, int a, int b, int c, int d, int n) {
+  double price[n + 1];
+
+  double max_el = price[1];
+  double max_decline = -1.0;
+
+  for (int k = 1; k <= n; k++) {
+    price[k] = p * (sin(a * k + b) + cos(c * k + d) + 2);
+    if (price[k] > max_el) max_el = price[k];
+    max_decline = fmax(max_el - price[k], max_decline);
+  }
+
+  return max_decline;
+}
--- artichoke/c/test_artichoke.c
+++ artichoke/c/test_artichoke.c
@@ -8,13 +8,28 @@ void tearDown(void) {}
 static void test_canary(void) { TEST_PASS(); }
 
 static void test_artichoke(void) {
-  TEST_ASSERT_DOUBLE_WITHIN(1e-6, 1.0, artichoke(1, 1, 1, 1, 1, 1));
+  double expected = 104.855110477;
+  TEST_ASSERT_DOUBLE_WITHIN(1e-6, expected, artichoke(42, 1, 23, 4, 8, 10));
+}
+
+static void test_artichoke_again(void) {
+  double expected = 0.00;
+  TEST_ASSERT_DOUBLE_WITHIN(1e-6, expected,
+                            artichoke(100, 7, 615, 998, 801, 3));
+}
+
+static void test_artichoke_thrice(void) {
+  double expected = 399.303813;
+  TEST_ASSERT_DOUBLE_WITHIN(1e-6, expected,
+                            artichoke(100, 432, 406, 867, 60, 1000));
 }
 
 int main(void) {
   UnityBegin("test_artichoke.c");
   RUN_TEST(test_canary);
   RUN_TEST(test_artichoke);
+  RUN_TEST(test_artichoke_again);
+  RUN_TEST(test_artichoke_thrice);
 
   return UnityEnd();
 }
