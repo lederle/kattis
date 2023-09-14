8ecd989b8d47de787589447c975572498591e923
Mike Lederle <mrlederle@gmail.com>
2023-09-14 16:20:31 -0400

    got it working

--- digits/c/digits.c
+++ digits/c/digits.c
@@ -1,8 +1,25 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "libdigits.h"
 
-//
+int main(void) {
+  char A[1000001];
+  while (scanf("%s", A) == 1) {
+    if (A[0] == 'E') return EXIT_SUCCESS;
 
-int main(void) { return EXIT_SUCCESS; }
+    int index = 2, next;
+    int curr = strlen(A);
+    while (1) {
+      next = 1.0 + floor(log10(curr));
+      if (curr == next) break;
+      index++;
+      curr = next;
+    }
+
+    printf("%d\n", index);
+  }
+  return EXIT_SUCCESS;
+}
