c4672e21dad68e089ef15eca9dc91a46fc03b20b
Mike Lederle <mrlederle@gmail.com>
2023-09-19 07:25:02 -0400

    test

--- filip/c/test_filip.c
+++ filip/c/test_filip.c
@@ -7,9 +7,16 @@ void tearDown(void) {}
 
 static void test_canary(void) { TEST_PASS(); }
 
+static void test_filip(void) {
+  int rev, A = 123;
+  filip(A, &rev);
+  TEST_ASSERT_EQUAL_INT(321, rev);
+}
+
 int main(void) {
   UnityBegin("test_filip.c");
   RUN_TEST(test_canary);
+  RUN_TEST(test_filip);
 
   return UnityEnd();
 }
